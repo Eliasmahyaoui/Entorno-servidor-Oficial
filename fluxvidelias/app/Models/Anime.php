adfeee140537ed90261f23e0cf2eb3515b364506
elias <eliasmahyaoui6@hotmail.com>
2025-12-16 20:45:11 +0100

    16/12/2025 - 20:45

--- fluxvidelias/app/Models/Anime.php
+++ fluxvidelias/app/Models/Anime.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Anime extends Model
 {
 
+    protected $hidden= ['created_at', 'updated_at'];
 
     public function getRouteKeyName()
     {
